4b54420edf12aac72c539f8dfc2ccaec0cf1ed47
Matthew Whitt <mwhitt.w@gmail.com>
2016-04-19 00:28:06 -0400

    add crud

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ var buildMongodbUrl = function() {
     db_url = "mongodb://localhost:27017/eversea";
   } else {
     db_url = process.env.MONGODB_URI;
+  }
   return db_url;
 }
 
@@ -58,41 +59,68 @@ function handleError(res, reason, message, code) {
  */
 
 app.get("/notes", function(req, res) {
+  db.collection(NOTES_COLLECTION).find({}).toArray(function(err, docs) {
+    if (err) {
+      handleError(res, err.message, "Failed to get notes.");
+    } else {
+      res.status(200).json(docs);
+    }
+  });
 });
-
 
 app.post("/notes", function(req, res) {
   var newNote = req.body;
-    newNote.createDate = new Date();
+  newNote.createDate = new Date();
 
-    if (!req.body.note) {
-      handleError(res, "Invalid user input", "Must provide a first or last name.", 400);
+  if (!req.body.note) {
+    handleError(res, "Invalid user input", "Must add a note.", 400);
+  }
+
+  db.collection(NOTES_COLLECTION).insertOne(newNote, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to create new note.");
+    } else {
+      res.status(201).json(doc.ops[0]);
     }
-
-    db.collection(NOTES_COLLECTION).insertOne(newNote, function(err, doc) {
-      if (err) {
-        handleError(res, err.message, "Failed to create new contact.");
-      } else {
-        res.status(201).json(doc.ops[0]);
-      }
-    });
+  });
 });
 
-
-/* /notes/:id
- * GET: find notes by id
- * PUT: update notes by id
- * DELETE: delete notes by id
+/*  "/notes/:id"
+ *    GET: find note by id
+ *    PUT: update note by id
+ *    DELETE: deletes note by id
  */
 
 app.get("/notes/:id", function(req, res) {
+  db.collection(NOTES_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to get note.");
+    } else {
+      res.status(200).json(doc);
+    }
+  });
 });
-
 
 app.put("/notes/:id", function(req, res) {
+  var updateDoc = req.body;
+  delete updateDoc._id;
+
+  db.collection(NOTES_COLLECTION).updateOne({_id: new ObjectID(req.params.id)}, updateDoc, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to update note.");
+    } else {
+      res.status(204).end();
+    }
+  });
 });
 
-
 app.delete("/notes/:id", function(req, res) {
+  db.collection(NOTES_COLLECTION).deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
+    if (err) {
+      handleError(res, err.message, "Failed to delete note.");
+    } else {
+      res.status(204).end();
+    }
+  });
 });
 
